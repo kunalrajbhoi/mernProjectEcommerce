71a6e051053dd8a80d96006f86464f885bd19426
kunalrajbhoi <kunalrajbhoi31@gmail.com>
2024-04-27 21:28:21 +0530

    update mongo db

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 async function connectDB() {
     try{
-        mongoose.connect('mongodb://127.0.0.1:27017/Ecommerce')
+        // mongoose.connect('mongodb://127.0.0.1:27017/Ecommerce')
+       await mongoose.connect(process.env.MONGODB_URI)
         console.log("mongo db is connect to node");
     }
     catch(err){
