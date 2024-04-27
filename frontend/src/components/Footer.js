0ef0bb6b4c0e4a343bc056a0eaf3cc90f358dc4e
kunalrajbhoi <kunalrajbhoi31@gmail.com>
2024-04-27 23:59:52 +0530

    updates

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <footer className='bg-slate-200'>
       <div className='container mx-auto p-4'>
-        <p className='text-center font-bold' title='kunal raj bhoi'>code with me</p>
+        <div className='flex justify-center items-center'>
+          <p className='text-center font-bold' title='kunal raj bhoi'>kunal raj bhoi</p>
+          <div className='ml-4'>
+            <a href='https://www.linkedin.com/in/kunal-raj-bhoi-ab8a791ba/' target='_blank' rel='noopener noreferrer' className='text-blue-500 hover:text-blue-700'>
+              <FaLinkedin size={20} />
+            </a>
+          </div>
+          <div className='ml-4'>
+            <a href='https://github.com/kunalrajbhoi' target='_blank' rel='noopener noreferrer' className='text-gray-600 hover:text-gray-800'>
+              <FaGithub size={20} />
+            </a>
+          </div>
+          <div className='ml-4'>
+            <p className='text-gray-600 hover:text-gray-800'>Contact: +9024186854</p>
+          </div>
+        </div>
       </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
